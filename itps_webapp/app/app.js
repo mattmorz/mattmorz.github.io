d67812bcea2142553c772d7245ffd30dfe97adf1
Matt O. Morales <idsilmat@gmail.com>
2020-12-01 10:41:19 +0800

    Update app.js

--- itps_webapp/app/app.js
+++ itps_webapp/app/app.js
@@ -1,45 +1,3 @@
-
-/*
- Copyright (c) 2013, Neil Jakeman
- GWC Layer implements access to a pre rendered GWC tile cache.
- NB. URL to take the form:
-  '{s}/path/to/cache/EPSG_900913_{z}/{dir_x}_{dir_y}/{x}_{y}.png';
- Options:
- Must declare {tms:true}
- 
-*/
-
-L.TileLayer.GWC = L.TileLayer.extend({
-
-	_padZeros: function(unPaddedInt,padReq) {
-			padded = unPaddedInt.toString()
-			while (padded.length < padReq) {
-					padded = '0'+padded;
-			}
-			return padded
-	},
-	
-	_formatZ: function(z) {
-			if (z<10) {
-				z='0'+z;
-			}
-			return z
-		},
-	
-	getTileUrl: function (tilePoint) {
-
-		return L.Util.template(this._url, L.extend({
-			s: this._getSubdomain(tilePoint),
-			z: this._formatZ(this._getZoomForUrl()),
-			dir_x: this._padZeros(Math.floor(tilePoint.x/(Math.pow(2,Math.floor(1+(this._getZoomForUrl(tilePoint)/2))))), Math.floor(this._getZoomForUrl(tilePoint)/6)+1),
-			dir_y: this._padZeros(Math.floor(tilePoint.y/(Math.pow(2,Math.floor(1+(this._getZoomForUrl(tilePoint)/2))))), Math.floor(this._getZoomForUrl(tilePoint)/6)+1),
-			x: this._padZeros(tilePoint.x,2+(Math.floor(this._getZoomForUrl(tilePoint)/6)*2)),
-			y: this._padZeros(tilePoint.y,2+(Math.floor(this._getZoomForUrl(tilePoint)/6)*2))
-			
-		}, this.options));
-	}
-})
-
 var LAYERS = [];
 var coverage_type = null;
 
@@ -83,7 +41,7 @@ var munRepoFalcata1 = null;
 var munRepoGmelina = L.geoJson(null);
 var munRepoGmelina1 = null;
 var munRepoMangium = L.geoJson(null);
-var munRepoMangium = null;
+var munRepoMangium1 = null;
 var munRepoBagras = L.geoJson(null);
 var munRepoBagras1 = null;
 
