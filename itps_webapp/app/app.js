327df3856b3c9ff7f97a5fcdd0a63ff2cfb673ba
Edsel Matt O. Morales <idsilmat@gmail.com>
2021-01-27 15:34:47 +0800

    Update app.js

--- itps_webapp/app/app.js
+++ itps_webapp/app/app.js
@@ -108,7 +108,7 @@ function toggleTrees(URL, treeType, checked) {
         //Download progress
         xhr.addEventListener("progress", function(evt) {
           console.log(evt.lengthComputable);
-          console.log(evt.loaded,evt.tota);
+          console.log(evt.loaded,evt.total);
          //if (evt.lengthComputable) {
             
             percentComplete = parseFloat(evt.loaded / evt.total) * 100;
